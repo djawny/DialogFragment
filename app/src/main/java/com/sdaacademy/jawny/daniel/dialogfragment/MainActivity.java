e581b8beb6598efa8915af0ea1ce668a0d220c5d
djawny <djawny@op.pl>
2017-02-08 19:49:12 +0100

    zmiana 08/02

--- app/src/main/java/com/sdaacademy/jawny/daniel/dialogfragment/MainActivity.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/dialogfragment/MainActivity.java
@@ -2,17 +2,20 @@ package com.sdaacademy.jawny.daniel.dialogfragment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.sdaacademy.jawny.daniel.dialogfragment.dialogs.RegistrationDialogFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.i(TAG, "onCreate");
     }
 
     public void clickRegister(View view) {
@@ -24,30 +27,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        Log.i(TAG, "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.i(TAG, "onResume");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
+        Log.i(TAG, "onRestart");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.i(TAG, "onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Log.i(TAG, "onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.i(TAG, "onDestroy");
     }
 }
