bc23d8991107b2fadccede4aac257789d402fcf0
djawny <djawny@op.pl>
2017-02-08 19:55:47 +0100

    zmiana 08/02

--- app/src/main/java/com/sdaacademy/jawny/daniel/dialogfragment/dialogs/RegistrationDialogFragment.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/dialogfragment/dialogs/RegistrationDialogFragment.java
@@ -3,6 +3,7 @@ package com.sdaacademy.jawny.daniel.dialogfragment.dialogs;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +12,41 @@ import com.sdaacademy.jawny.daniel.dialogfragment.R;
 
 public class RegistrationDialogFragment extends DialogFragment {
 
+    private static final String TAG = RegistrationDialogFragment.class.getSimpleName();
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.dialog_register, container);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
     }
 }
